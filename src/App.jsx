e29012724ffacccb00e5d6712ba60c46eb5e9373
rpatel25 <33490660+rpatel25@users.noreply.github.com>
2023-12-11 14:47:14 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Header from './components/Header.jsx';
+import { CORE_CONCEPTS } from './data.js';
+
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.description} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <section id="core-concepts">
+        <main>
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcepts
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </main>
+      </section>
+    </div>
+  );
+}
+
+export default App;
