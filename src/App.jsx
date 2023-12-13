9142cf5062e0e47cd91493f6e8f1ad4f418caf31
rpatel25 <33490660+rpatel25@users.noreply.github.com>
2023-12-13 15:34:15 -0500

    Final code for React Essentials (Core Concepts)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import Header from './components/Header.jsx';
-import { CORE_CONCEPTS } from './data.js';
-
-function CoreConcepts(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.description} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import { useState } from 'react';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a tab content!</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Header />
-      <section id="core-concepts">
-        <main>
+      <main>
+        <Header />
+        <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcepts
-              image={CORE_CONCEPTS[0].image}
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-            />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
-        </main>
-      </section>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onSelect={() => handleSelect('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onSelect={() => handleSelect('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onSelect={() => handleSelect('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onSelect={() => handleSelect('state')}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import reactImage from '../assets/react-core-concepts.png';
+import reactImage from '../../assets/react-core-concepts.png';
+import './header.css';
 
 const reactDescriptions = ['Fundamental', 'Critical', 'Core'];
 
